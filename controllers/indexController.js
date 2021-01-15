f5a99b147d2616efc74df29859e307742f99b0aa
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-01-15 11:21:17 -0300

    cambios en el home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,8 @@
 module.exports = {
     index:(req,res) =>{
         res.render('home',{
-            title : "Home",
-            mensaje: "Bienvenidos a mi Mercado Autos S.A."
+            title : "Mercado Autos S.A.",
+            mensaje: "Solo tenés que avanzar..."
         })
     },
   
